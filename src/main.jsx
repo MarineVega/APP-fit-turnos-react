03133abdc37ce79127e8c090aa7ffd9bbb7ad3a7
nataliagelmi <nataliagelmi@gmail.com>
2025-10-06 14:36:26 -0300

    se saca carpetas que no use

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/style.css";
-import { AuthProvider } from "./context/AuthContext.jsx"; // 👈 importá el provider
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      
         <App />
-      </AuthProvider>
+      
     </BrowserRouter>
   </React.StrictMode>
 );

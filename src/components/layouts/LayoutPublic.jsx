ba6525b2c813c0653f79aad42b328d77da03cafb
nataliagelmi <nataliagelmi@gmail.com>
2025-11-21 20:20:22 -0300

    cambio style y el estilo en layout

--- src/components/layouts/LayoutPublic.jsx
+++ src/components/layouts/LayoutPublic.jsx
@@ -13,7 +13,7 @@ export default function LayoutPublic() {
   }, []);
 
   return (
-    <div className="layout-public">
+    <div className="App">
       <Header />
       <main className="contenido">
         <Outlet />
